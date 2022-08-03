c0a137ff012230189e9d7d8d511efd6a76568915
OmerBenGera <omerthepro126@gmail.com>
2022-08-03 19:30:05 +0300

    Added nms utilities to better detect incorrect usage of nms

---
+++ src/main/java/com/bgsoftware/wildloaders/nms/mapping/RemapFailure.java
@@ -0,0 +1,9 @@
+package com.bgsoftware.wildloaders.nms.mapping;
+
+public class RemapFailure extends RuntimeException {
+
+    public RemapFailure(String message) {
+        super(message);
+    }
+
+}
