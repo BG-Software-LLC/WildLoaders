0c5c2cec27fd1105ce38795615b09d66bf2e965f
nivcoo <nico64800@live.fr>
2025-08-23 22:35:43 +0200

    improv. change pair class to DatabaseEntry class

--- API/src/main/java/com/bgsoftware/wildloaders/api/data/DatabaseFilter.java
+++ API/src/main/java/com/bgsoftware/wildloaders/api/data/DatabaseFilter.java
@@ -2,7 +2,7 @@ package com.bgsoftware.wildloaders.api.data;
 
 
 
-import com.bgsoftware.wildloaders.api.objects.Pair;
+import com.bgsoftware.wildloaders.api.objects.DatabaseEntry;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -17,14 +17,14 @@ public abstract class DatabaseFilter {
         return new DatabaseFilterSingle(filterKey, filterValue);
     }
 
-    public static DatabaseFilter fromFilters(List<Pair<String, Object>> filters) {
+    public static DatabaseFilter fromFilters(List<DatabaseEntry<String, Object>> filters) {
         if (filters.isEmpty()) {
             if (EMPTY_FILTER == null)
                 EMPTY_FILTER = new DatabaseFilterEmpty();
 
             return EMPTY_FILTER;
         } else if (filters.size() == 1) {
-            Pair<String, Object> filter = filters.get(0);
+            DatabaseEntry<String, Object> filter = filters.get(0);
             return fromFilter(filter.getKey(), filter.getValue());
         } else {
             return new DatabaseFilterList(filters);
@@ -36,23 +36,23 @@ public abstract class DatabaseFilter {
 
     public abstract void forEach(BiConsumer<String, Object> consumer);
 
-    public abstract Collection<Pair<String, Object>> getFilters();
+    public abstract Collection<DatabaseEntry<String, Object>> getFilters();
 
     private static class DatabaseFilterList extends DatabaseFilter {
 
-        private final Collection<Pair<String, Object>> filters;
+        private final Collection<DatabaseEntry<String, Object>> filters;
 
-        DatabaseFilterList(Collection<Pair<String, Object>> filters) {
+        DatabaseFilterList(Collection<DatabaseEntry<String, Object>> filters) {
             this.filters = filters;
         }
 
         @Override
         public void forEach(BiConsumer<String, Object> consumer) {
-            filters.forEach(pair -> consumer.accept(pair.getKey(), pair.getValue()));
+            filters.forEach(databaseEntry -> consumer.accept(databaseEntry.getKey(), databaseEntry.getValue()));
         }
 
         @Override
-        public Collection<Pair<String, Object>> getFilters() {
+        public Collection<DatabaseEntry<String, Object>> getFilters() {
             return Collections.unmodifiableCollection(filters);
         }
 
@@ -66,7 +66,7 @@ public abstract class DatabaseFilter {
         }
 
         @Override
-        public Collection<Pair<String, Object>> getFilters() {
+        public Collection<DatabaseEntry<String, Object>> getFilters() {
             return Collections.emptyList();
         }
 
@@ -88,8 +88,8 @@ public abstract class DatabaseFilter {
         }
 
         @Override
-        public Collection<Pair<String, Object>> getFilters() {
-            return Collections.singleton(new Pair<>(filterKey, filterValue));
+        public Collection<DatabaseEntry<String, Object>> getFilters() {
+            return Collections.singleton(new DatabaseEntry<>(filterKey, filterValue));
         }
 
     }
