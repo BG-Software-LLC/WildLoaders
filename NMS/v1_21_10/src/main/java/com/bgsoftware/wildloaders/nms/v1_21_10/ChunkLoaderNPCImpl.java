a36e621c2a12751847748feee0f8360524e5be5f
OmerBenGera <omerthepro126@gmail.com>
2025-12-18 21:08:53 +0200

    Adds support to 1.21.11

---
+++ NMS/v1_21_10/src/main/java/com/bgsoftware/wildloaders/nms/v1_21_10/ChunkLoaderNPCImpl.java
@@ -0,0 +1,226 @@
+package com.bgsoftware.wildloaders.nms.v1_21_10;
+
+import com.bgsoftware.wildloaders.handlers.NPCHandler;
+import com.bgsoftware.wildloaders.npc.DummyChannel;
+import com.mojang.authlib.GameProfile;
+import net.minecraft.advancements.AdvancementHolder;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.network.DisconnectionDetails;
+import net.minecraft.network.PacketListener;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.PacketFlow;
+import net.minecraft.network.protocol.game.ServerboundChatPacket;
+import net.minecraft.network.protocol.game.ServerboundContainerClickPacket;
+import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
+import net.minecraft.network.protocol.game.ServerboundPlayerActionPacket;
+import net.minecraft.network.protocol.game.ServerboundSetCarriedItemPacket;
+import net.minecraft.network.protocol.game.ServerboundSignUpdatePacket;
+import net.minecraft.network.protocol.game.ServerboundUseItemPacket;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.PlayerAdvancements;
+import net.minecraft.server.ServerAdvancementManager;
+import net.minecraft.server.level.ClientInformation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.CommonListenerCookie;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.storage.LevelResource;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.UUID;
+
+public class ChunkLoaderNPCImpl extends com.bgsoftware.wildloaders.nms.v1_21_10.AbstractChunkLoaderNPC {
+
+    private NPCServerPlayer serverPlayer;
+
+    public ChunkLoaderNPCImpl(Location location, UUID uuid) {
+        super(location, uuid);
+    }
+
+    @Override
+    protected ServerPlayer createServerPlayer(ServerLevel serverLevel, UUID uuid) {
+        return (this.serverPlayer = new NPCServerPlayer(serverLevel, uuid));
+    }
+
+    @Override
+    protected ServerGamePacketListenerImpl createDummyServerGamePacketListenerImpl() {
+        return new DummyServerGamePacketListenerImpl(MinecraftServer.getServer(), this.serverPlayer);
+    }
+
+    @Override
+    protected void setViewDistanceInternal(int viewDistance) {
+        CraftPlayer craftPlayer = this.serverPlayer.getBukkitEntity();
+        craftPlayer.setSendViewDistance(2);
+        craftPlayer.setViewDistance(2);
+        craftPlayer.setSimulationDistance(2);
+    }
+
+    @Override
+    protected void movePlayerInternal(double x, double y, double z, float yaw, float pitch) {
+        this.serverPlayer.snapTo(x, y, z, yaw, pitch);
+    }
+
+    @Override
+    protected void removePlayerInternal(Entity.RemovalReason removalReason) {
+        this.serverPlayer.removeInternal(removalReason);
+    }
+
+    private class NPCServerPlayer extends ServerPlayer {
+
+        private final PlayerAdvancements advancements = new DummyPlayerAdvancements(MinecraftServer.getServer(), this);
+
+        NPCServerPlayer(ServerLevel serverLevel, UUID uuid) {
+            super(MinecraftServer.getServer(), serverLevel,
+                    new GameProfile(uuid, NPCHandler.getName(serverLevel.getWorld().getName())),
+                    ClientInformation.createDefault());
+        }
+
+        @Override
+        public void remove(@NotNull RemovalReason reason) {
+            ChunkLoaderNPCImpl.this.handlePlayerRemove(reason);
+        }
+
+        @Override
+        public @NotNull PlayerAdvancements getAdvancements() {
+            return this.advancements;
+        }
+
+        private void removeInternal(RemovalReason reason) {
+            super.remove(reason);
+        }
+
+    }
+
+    public static class DummyConnection extends net.minecraft.network.Connection {
+
+        DummyConnection() {
+            super(PacketFlow.SERVERBOUND);
+            this.channel = new DummyChannel();
+            this.address = null;
+        }
+
+        @Override
+        public void setListenerForServerboundHandshake(@NotNull PacketListener packetListener) {
+            // Do nothing.
+        }
+    }
+
+    public static class DummyServerGamePacketListenerImpl extends ServerGamePacketListenerImpl {
+
+        DummyServerGamePacketListenerImpl(MinecraftServer minecraftServer, ServerPlayer serverPlayer) {
+            super(minecraftServer, new DummyConnection(), serverPlayer,
+                    CommonListenerCookie.createInitial(serverPlayer.getGameProfile(), false));
+        }
+
+        @Override
+        public void handleContainerClick(@NotNull ServerboundContainerClickPacket containerClickPacket) {
+            // Do nothing.
+        }
+
+        @Override
+        public void handleMovePlayer(@NotNull ServerboundMovePlayerPacket movePlayerPacket) {
+            // Do nothing.
+        }
+
+        @Override
+        public void handleSignUpdate(@NotNull ServerboundSignUpdatePacket signUpdatePacket) {
+            // Do nothing.
+        }
+
+        @Override
+        public void handlePlayerAction(@NotNull ServerboundPlayerActionPacket playerActionPacket) {
+            // Do nothing.
+        }
+
+        @Override
+        public void handleUseItem(@NotNull ServerboundUseItemPacket useItemPacket) {
+            // Do nothing.
+        }
+
+        @Override
+        public void handleSetCarriedItem(@NotNull ServerboundSetCarriedItemPacket setCarriedItemPacket) {
+            // Do nothing.
+        }
+
+        @Override
+        public void handleChat(@NotNull ServerboundChatPacket chatPacket) {
+            // Do nothing.
+        }
+
+        @Override
+        public void disconnectAsync(DisconnectionDetails disconnectionInfo) {
+            // Do nothing.
+        }
+
+        public void send(@NotNull Packet<?> packet) {
+            // Do nothing.
+        }
+
+    }
+
+    private static class DummyPlayerAdvancements extends PlayerAdvancements {
+
+        DummyPlayerAdvancements(MinecraftServer server, ServerPlayer serverPlayer) {
+            super(server.getFixerUpper(), server.getPlayerList(), server.getAdvancements(),
+                    getAdvancementsFile(server, serverPlayer), serverPlayer);
+        }
+
+        private static Path getAdvancementsFile(MinecraftServer server, ServerPlayer serverPlayer) {
+            File advancementsDir = server.getWorldPath(LevelResource.PLAYER_ADVANCEMENTS_DIR).toFile();
+            return new File(advancementsDir, serverPlayer.getUUID() + ".json").toPath();
+        }
+
+        @Override
+        public void setPlayer(@NotNull ServerPlayer owner) {
+            // Do nothing.
+        }
+
+        @Override
+        public void stopListening() {
+            // Do nothing.
+        }
+
+        @Override
+        public void reload(@NotNull ServerAdvancementManager advancementLoader) {
+            // Do nothing.
+        }
+
+        @Override
+        public void save() {
+            // Do nothing.
+        }
+
+        @Override
+        public boolean award(@NotNull AdvancementHolder advancement, @NotNull String criterionName) {
+            return false;
+        }
+
+        @Override
+        public boolean revoke(@NotNull AdvancementHolder advancement, @NotNull String criterionName) {
+            return false;
+        }
+
+        @Override
+        public void flushDirty(@NotNull ServerPlayer player, boolean showAdvancements) {
+            // Do nothing.
+        }
+
+        @Override
+        public void setSelectedTab(@Nullable AdvancementHolder advancement) {
+            // Do nothing.
+        }
+
+        @Override
+        public @NotNull AdvancementProgress getOrStartProgress(@NotNull AdvancementHolder advancement) {
+            return new AdvancementProgress();
+        }
+
+    }
+
+}
---
+++ NMS/v1_21_10/src/main/java/com/bgsoftware/wildloaders/nms/v1_21_10/NMSAdapterImpl.java
@@ -0,0 +1,78 @@
+package com.bgsoftware.wildloaders.nms.v1_21_10;
+
+import com.bgsoftware.common.reflection.ReflectField;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
+import com.mojang.authlib.properties.Property;
+import com.mojang.authlib.properties.PropertyMap;
+import com.mojang.datafixers.util.Either;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.entity.player.PlayerSkin;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.CustomData;
+import net.minecraft.world.item.component.ResolvableProfile;
+
+import java.lang.reflect.Modifier;
+import java.util.Optional;
+
+public class NMSAdapterImpl extends com.bgsoftware.wildloaders.nms.v1_21_10.AbstractNMSAdapter {
+
+    private static final ReflectField<CompoundTag> CUSTOM_DATA_TAG = new ReflectField<>(CustomData.class,
+            CompoundTag.class, Modifier.PRIVATE | Modifier.FINAL, 1);
+
+    @Override
+    protected String getTagInternal(ItemStack itemStack, String key, String def) {
+        CustomData customData = itemStack.get(DataComponents.CUSTOM_DATA);
+        if (customData != null) {
+            CompoundTag compoundTag = getCustomDataTag(customData);
+            return compoundTag.getStringOr(key, def);
+        }
+        return def;
+    }
+
+    @Override
+    protected void setTagInternal(ItemStack itemStack, String key, String value) {
+        CustomData customData = itemStack.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY);
+        customData = customData.update(compoundTag -> compoundTag.putString(key, value));
+        itemStack.set(DataComponents.CUSTOM_DATA, customData);
+    }
+
+    @Override
+    protected long getTagInternal(ItemStack itemStack, String key, long def) {
+        CustomData customData = itemStack.get(DataComponents.CUSTOM_DATA);
+        if (customData != null) {
+            CompoundTag compoundTag = getCustomDataTag(customData);
+            return compoundTag.getLongOr(key, def);
+        }
+        return def;
+    }
+
+    @Override
+    protected void setTagInternal(ItemStack itemStack, String key, long value) {
+        CustomData customData = itemStack.getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY);
+        customData = customData.update(compoundTag -> compoundTag.putLong(key, value));
+        itemStack.set(DataComponents.CUSTOM_DATA, customData);
+    }
+
+    @Override
+    protected void setTextureForItem(ItemStack itemStack, String texture) {
+        Multimap<String, Property> properties = HashMultimap.create();
+        properties.put("textures", new Property("textures", texture));
+
+        ResolvableProfile.Partial partialProfile = new ResolvableProfile.Partial(
+                Optional.empty(), Optional.empty(), new PropertyMap(properties));
+        ResolvableProfile resolvableProfile = new ResolvableProfile.Static(Either.right(partialProfile), PlayerSkin.Patch.EMPTY);
+
+        itemStack.set(DataComponents.PROFILE, resolvableProfile);
+    }
+
+    private static CompoundTag getCustomDataTag(CustomData customData) {
+        try {
+            return customData.getUnsafe();
+        } catch (Throwable error) {
+            return CUSTOM_DATA_TAG.get(customData);
+        }
+    }
+
+}
