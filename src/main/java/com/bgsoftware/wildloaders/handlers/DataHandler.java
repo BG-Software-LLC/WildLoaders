905cbf85a3e87683a934dd3d7bb1f3867b7a6ed7
Lazar Bogićević <bogicevic.pr133.2024@uns.ac.rs>
2026-07-11 15:55:40 +0200

    Fix deadlock during database load by checking chunk loaded state (#158)

--- src/main/java/com/bgsoftware/wildloaders/handlers/DataHandler.java
+++ src/main/java/com/bgsoftware/wildloaders/handlers/DataHandler.java
@@ -79,8 +79,10 @@ public final class DataHandler {
 
                 if (world != null) {
                     Location location = blockPosition.getLocation();
-                    Scheduler.ensureMain(location, () -> plugin.getLoaders().addChunkLoaderWithoutDBSave(
-                            loaderData.get(), placer, location, timeLeft, true, null));
+                    Scheduler.runTaskLater(() -> {
+                    plugin.getLoaders().addChunkLoaderWithoutDBSave(
+                            loaderData.get(), placer, location, timeLeft, true, null);
+                    }, 1L);
                 } else {
                     plugin.getLoaders().addUnloadedChunkLoader(loaderData.get(), placer, blockPosition, timeLeft);
                 }
